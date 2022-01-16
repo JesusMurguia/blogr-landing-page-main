2c20579565a52b37e3879f85193568837d114b94
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-01-16 08:12:22 -0700

    menu js

--- js/index.js
+++ js/index.js
@@ -3,10 +3,18 @@ window.onload = function() {
     let btnMenu = document.getElementById('btn-menu');
     let menuImg = document.getElementById('img-menu');
     let mobileMenu = document.getElementById('mobile-menu');
+    let mobileMenuItems = document.getElementsByClassName('mobile-btn');
 
     btnMenu.addEventListener('click', function() {
         menuImg.classList.toggle('menu-active');
         mobileMenu.classList.toggle('hidden');
     });
+
+    for (let i = 0; i < mobileMenuItems.length; i++) {
+        mobileMenuItems[i].addEventListener('click', function() {
+            menuImg.classList.toggle('menu-active');
+            mobileMenu.classList.toggle('hidden');
+        });
+    }
     
 }
